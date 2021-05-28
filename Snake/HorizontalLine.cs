72d28e190e73c55f6811c9a07bc1747670212df1
Pavel <tursunkulov163@yandex.ru>
2021-05-28 23:33:09 +0300

    Added Figure. Learned principal nasledovanie

--- Snake/HorizontalLine.cs
+++ Snake/HorizontalLine.cs
@@ -4,9 +4,8 @@ using System.Text;
 
 namespace Snake
 {
-    class HorizontalLine
+    class HorizontalLine : Figure
     {
-        List<Point> pList;
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
@@ -14,13 +13,6 @@ namespace Snake
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
-            }
-        }
-        public void Drow()
-        {
-            foreach(Point p in pList)
-            {
-                p.Draw();
             }
         }
     }
